ad7fb10db7d6c9c1550f0cb3dd098a5da6b78dab
ivanefr <efrulka2009@gmail.com>
2024-02-02 17:22:24 +0300

    commit

--- main.py
+++ main.py
@@ -2,19 +2,37 @@ from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 import sys
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
+import requests
 
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.image_path = "data/image.png"
 
     def initUI(self):
         uic.loadUi("main.ui", self)
         self.search_but.clicked.connect(self.search_click)
 
     def search_click(self):
-        self.line_edit.setText("blablablablbalbl")
+        self.get_image()
+        pixmap = QPixmap(self.image_path)
+        self.label.setPixmap(pixmap)
+
+    def get_image(self):
+        geocode = self.line_edit.text()
+        api_server = "http://static-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": geocode,
+            # пропиши здесь все параметры пжпжпжпжпжпж
+        }
+
+        response = requests.get(api_server, params=params)
+
+        with open(self.image_path, "wb") as file:
+            file.write(response.content)
 
 
 if __name__ == "__main__":
