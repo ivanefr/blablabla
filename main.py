45c6050e7706d8f204342a6ccfa92e8e498a091d
ivanefr <efrulka2009@gmail.com>
2024-02-02 17:00:27 +0300

    blablablablablblalbal

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-class Window():
-    ...
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+import sys
+from PyQt5.QtGui import QPixmap
+from PyQt5 import uic
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().init()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi("main.ui", self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec_())
